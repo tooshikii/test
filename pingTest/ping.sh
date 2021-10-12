3ddc29557c53966dc282682979857cc9e1fd9675
tooshikii <s.ikihsot@gmail.com>
2021-10-12 09:24:08 +0200

    Initial commit

---
+++ pingTest/ping.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Usage: ./monitor-timeouts.sh [duration] [target]
+#example: ./monitor-timeouts.sh 60 192.168.1.1
+minutes=1440
+target=$2
+if [ $# -eq 0 ]; then
+    minutes=1
+    target=google.com
+fi
+if [ -z "$2" ]; then
+    target=google.com
+fi
+pings=$((60 * $minutes))
+
+system=`uname`
+if [[ $system == *"Linux"* ]]; then
+    extraflag="-O"
+fi
+
+echo "Start monitor for network timeouts at `date` for $minutes minute(s)."
+echo "Target host: $target"
+ping $target -i 1 -c $pings $extraflag | grep -i "timeout\|unreachable\|no answer\|statistics\|transmitted\|avg"
+echo "End monitoring at `date`."
