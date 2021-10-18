1d392d4ea2f9a82ea54fb6e52db108141518d805
tooshikii <s.ikihsot@gmail.com>
2021-10-18 16:20:28 +0200

    Add billing API

--- notes/stacks/index.js
+++ notes/stacks/index.js
@@ -5,8 +5,6 @@ import AuthStack from "./AuthStack";
 export default function main(app) {
   const storageStack =  new StorageStack(app, 'storage');
 
-  console.log({storageStack})
-
   const apistack = new ApiStack(app, 'api', {
     table : storageStack.table
   })
